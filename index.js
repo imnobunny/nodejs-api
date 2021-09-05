1b13a89df2b75a11f21eff67d1ed767342dbd985
Saira <32007478+imnobunny@users.noreply.github.com>
2021-09-05 16:45:42 +0800

    refactor!:  refactor db url

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ require('dotenv').config();
 
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri ='mongodb+srv://admin:admin1234@cbk.p9e2b.mongodb.net/redvelvet?retryWrites=true&w=majority';
+const databaseUrl =process.env.DATABASE_URL || 'mongodb+srv://admin:admin1234@cbk.p9e2b.mongodb.net/redvelvet?retryWrites=true&w=majority';
 
-mongoose.connect(uri, { 
+mongoose.connect(databaseUrl, { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+}); 
 
 const connection = mongoose.connection;
 connection.once('open', () => {
