40489e3eb02739566cffdc5d343fe6eaf5a99120
Saira <32007478+imnobunny@users.noreply.github.com>
2021-09-05 19:59:10 +0800

    feat: added update rote for user

--- routes/users.js
+++ routes/users.js
@@ -113,7 +113,26 @@ router.route('/delete/:id').delete((req, res) => {
     }
 });
     
-   
+router.route('/update/:_id').patch((req, res) => {
+   try {
+
+    User.findOneAndUpdate(req.params, req.body, {upsert: true}, function(err, doc) {
+       
+        if (req.body?.password) {
+            // hash password
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                req.body.password = hash;
+            });
+
+        }
+        if (err) return res.status(400).json({ success: false, message: err });
+        return res.json({ success: true });
+    });
+
+   } catch (err) {
+        res.status(400).json({ success: false, message: err });
+   }
+});
 
 
 
